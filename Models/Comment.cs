f2c8d78065e0a85e54959fd834de52b888e80953
WangariNelly <nellywangari30@gmail.com>
2024-11-29 19:17:51 +0300

    Creating the models for the relationships for the data

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,11 @@ namespace LocalRecipes.Models
 {
     public class Comment
     {
-        
+        [Key]
+        public Guid CommentId { get; set; }
+         public Guid RecipeId { get; set; }
+          public Guid UserId { get; set; }
+          public string Text { get; set; } = string.Empty;
+          public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,14 @@ namespace LocalRecipes.Models
 {
     public class User
     {
-        
-    }
+        [Key]
+        public Guid UserId { get; set; }
+        public string Userame { get; set; } = string.Empty;   
+        public string Email { get; set; } = string.Empty;
+        public  string PasswordHash { get; set; } = string.Empty;
+        public string? Bio { get; set; } 
+        public string? ProfilePicture { get; set; } 
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    } 
 }
